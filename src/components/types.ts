e4f8ce3f17c0fa8e2b2441079d7f588528779202
Sayed <sayedshehata33@gmail.com>
2024-11-13 18:23:18 +0200

    feat: :sparkles: Format the Code and Add the Types

---
+++ src/components/types.ts
@@ -0,0 +1,16 @@
+export interface FormValues {
+    title: string;
+    name: string;
+    age: number;
+    email: string;
+    phone: string;
+}
+
+export interface Card {
+    title: string;
+    name: string;
+    age: number;
+    email: string;
+    phone: string;
+    status: string;
+}
